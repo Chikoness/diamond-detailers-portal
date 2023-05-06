46d82e91ac6b99cfd68a1da1dd2e136c60a83389
Charlene A <chichiko90@gmail.com>
2023-05-07 02:16:52 +0800

    Added cars constant data

--- src/data/cars.js
+++ src/data/cars.js
@@ -5,8 +5,12 @@ export const cars = {
     [`perodua-myvi`]: "Perodua Myvi",
     [`perodua-activa`]: "Perodua Ativa",
     [`honda-jazz`]: "Honda Jazz",
-    [`honda-crv`]: "Honda Crv",
+    [`honda-crv`]: "Honda CR-V",
     [`honda-city`]: "Honda City",
     [`honda-civic`]: "Honda Civic",
     [`toyota-avanza`]: "Toyota Avanza",
+    ['toyota-xtrail']: "Toyota X-Trail",
+    ['toyota-camry']: "Toyota Camry",
+    ['nissan-almera']: "Nissan Almera",
+    ['mitsubushi-pajero']: "Mitsubishi Pajero"
 }
