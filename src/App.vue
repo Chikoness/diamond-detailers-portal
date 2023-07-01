a3e504e568ca03b928f2dfae532d66c2b3a59633
Charlene A <chichiko90@gmail.com>
2023-07-01 19:47:21 +0800

    WIP testing capacitor's geolocation

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { Geolocation } from '@capacitor/geolocation';
 import BreadcrumbsBar from './components/BreadcrumbsBar.vue';
 
 export default {
@@ -14,6 +15,13 @@ export default {
     BreadcrumbsBar,
     IonApp,
     IonRouterOutlet
+  },
+
+  async mounted() {
+    const coord = await Geolocation.getCurrentPosition();
+
+    console.log(coord.coords.latitude);
+    console.log("HELLO!!!!!!")
   }
 }
 </script>
